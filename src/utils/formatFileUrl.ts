bb92e5cb37f5e4e1141f68dde5925c517b7a7232
lemon <lemon@example.com>
2025-04-09 04:43:38 +0300

    no code

--- src/utils/formatFileUrl.ts
+++
@@ -1,8 +0,0 @@
-import { prod } from "@/utils/prod"
-
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-
-export const formatFileUrl = (fileUrl: string) => {
-  if (!fileUrl) return `${BASE_URL}${prod}/media/images/unknown/empty.png`;
-  return fileUrl.startsWith("http") ? fileUrl : `${BASE_URL}${fileUrl}`;
-};
--- src/utils/prod.ts
+++
@@ -1,2 +0,0 @@
-export const prod =
-process.env.NEXT_PUBLIC_API_BASE_URL == "https://kid-wlsf.onrender.com" ? "/static" : "";
